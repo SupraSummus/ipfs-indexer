ed824d0d490bd6787e9dad1c09bb4c9db6b8151d
Jan Rydzewski <flegmer@gmail.com>
2018-10-01 20:30:13 +0200

    wip

--- db/kv.py
+++ db/kv.py
@@ -2,6 +2,121 @@ import urllib.parse
 
 
 class KV:
+    INITIAL_NODE_HASH = 'QmdfTbBqBPQ7VNxZEYEj14VmRuZBkqFbiwReogJgS1zR1n'
+    CHILD_LINK_PREFIX = '_'
+    KEY_LENGTH_LINK_NAME = 'k'
+    MAX_CHILDREN_COUNT = 256  # this musnt be less than 256
+    MAX_KEY_LENGTH = 64
+
+    def __init__(self, ipfs_client, state_hash):
+        self.__state_hash = state_hash
+        self.__ipfs_client = ipfs_client
+
+    @property
+    def state_hash(self):
+        return self.__state_hash
+
+    @property
+    def __key_length(self):
+        key_obj = self.__links.get(KEY_LENGTH_LINK_NAME)
+        if key_obj is None:
+            return self.MAX_KEY_LENGTH
+        else:
+            return int(self.__ipfs_client.get_object_data(key_obj))
+
+    @property
+    def __links(self):
+        return {
+            link['Name']: link['Hash']
+            for link in self.__client.object_links(self.__root).get('Links', [])
+        }
+
+    @property
+    def __children(self):
+        return {
+            self.__unescape_key(k): v
+            for k, v in self.__links
+            if k.startswith(CHILD_LINK_PREFIX)
+        }
+
+    def __escape_key(self, key):
+        return KEY_LINK_PREFIX + urllib.parse.quote_from_bytes(key, safe=' ')
+
+    def __unescape_key(self, key):
+        assert key.startswith(KEY_LINK_PREFIX)
+        return urllib.parse.unquote_to_bytes(key[len(KEY_LINK_PREFIX):])
+
+def __normalized(client, root):
+    children = _children(client, root)
+
+    if len(children) == 0:
+        return _INITIAL_NODE_HASH
+
+    elif len(children) == 1 and __key_length(client, root) < MAX_KEY_LENGTH:
+        key, child = children.items()[0]
+        if len(key) < __key_length(client, root):
+            # this only entry is a leaf
+            return root
+        return __normalized(client, prefix_keys(client, child, key))
+
+    elif len(children) > MAX_CHILDREN_COUNT:
+        # determine max prefix len so child count will be <= MAX_CHILDREN_COUNT
+        for prefix_len in reversed(range(1, __key_length(client, root))):
+            prefixes = set(k[:prefix_len] for k in __children(client, root))
+            if len(prefixes) <= MAX_CHILDREN_COUNT:
+                break
+
+        new_root = INITIAL_NODE_HASH
+        new_root = _set_link(client, new_root, KEY_LENGTH_LINK_NAME, client.object_patch_set_data(
+            EMPTY_OBJECT, b'{}'.format(prefix_len),
+        ))
+        for k, v in __children(client, root):
+            new_root = _set
+        
+
+
+def kv_empty(client):
+    return _INITIAL_NODE_HASH
+
+
+def kv_set(client, root, key, value):
+    key_length = _key_length(client, root)
+
+    if len(key) < key_length:
+        if value is None:
+            new_root = client.object_patch_rm_link(root, _escape_key(key))['Hash']
+        else:
+            new_root = client.object_patch_add_link(root, _escape_key(key), value)['Hash']
+
+    else:
+        key_head = _escape_key(key[:key_length])
+        key_tail = key[key_length:]
+
+        links = _links(client, root)
+        if key_head in links:
+            next_parent_hash = links[key_head]
+        else:
+            next_parent_hash = INITIAL_NODE_HASH
+
+        next_parent_hash = kv_set(client, next_parent_hash, key_tail, value)
+        if next_parent_hash == INITIAL_NODE_HASH:
+            new_root = client.object_patch_rm_link(
+                parent_hash,
+                key_part,
+            )['Hash']
+        else:
+            new_root = client.object_patch_add_link(
+                parent_hash,
+                key_part,
+                next_parent_hash,
+            )['Hash']
+
+    if new_root != root:
+        new_root = _normalize(client, new_root)
+    return new_root
+
+
+class KV:
     """Map-like object that holds mutable mapping bytestring -> ipfs object hash."""
     INITIAL_NODE_HASH = 'QmdfTbBqBPQ7VNxZEYEj14VmRuZBkqFbiwReogJgS1zR1n'
     CHILD_LINK_PREFIX = 'c'
@@ -111,8 +226,11 @@ class KV:
                 for sk in self.__keys(links[k], start, new_prefix):
                     yield sk
 
-    def merge(self, other_root, merge_func=lambda a, b: b):
-        raise NotImplementedError()
+    def union(self, other_root, merge_func=lambda a, b: b):
+        
+
+    def __union(self, root_a, root_b):
+        
 
 
 if __name__ == '__main__':
